b1d8934efba658f8a801040e833212c80c644e4d
HUU <huuki0726@icloud.com>
2025-09-22 16:53:17 +0900

    test commit

--- DirectX_FrameWork1/Source/01-Main/main.cpp
+++ DirectX_FrameWork1/Source/01-Main/main.cpp
@@ -14,6 +14,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, 
 
 	Application& app = Application::GetInstance();
 	app.Init(hInstance, nCmdShow);
+	std::cout << "Test push" << std::endl;
 	app.Run();
 	return app.UnInit();;
 }
