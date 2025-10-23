5567caf276cb908d151778d5391360d42b9a48aa
huuki <huuki0726@icloud.com>
2025-10-23 17:25:29 +0900

    カメラの挙動確認

--- DirectX_FrameWork1/Source/99-Lib/01-MyLib/08-Scene/01-Scenes/TitleScene.cpp
+++ DirectX_FrameWork1/Source/99-Lib/01-MyLib/08-Scene/01-Scenes/TitleScene.cpp
@@ -43,8 +43,8 @@ void TitleScene::Init()
 		}
 	}
 
-	camera2D.GetComponent<Camera2D>()->SetTarget(&gameObject2D);
-	camera3D.GetComponent<Camera3D>()->SetTarget(&gameObject3D);
+	//camera2D.GetComponent<Camera2D>()->SetTarget(&gameObject2D);
+	//camera3D.GetComponent<Camera3D>()->SetTarget(&gameObject3D);
 }
 
 void TitleScene::Input()
@@ -59,22 +59,32 @@ void TitleScene::Update()
 	
 	{
 		Transform* p_trf = camera3D.GetTransformPtr();
-		if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
-			p_trf->position.x += 0.001f;
-		if (GetAsyncKeyState(VK_LEFT) & 0x8000)
-			p_trf->position.x -= 0.001f;
+		if (GetAsyncKeyState('D') & 0x8000)
+			p_trf->position.x += 0.04f;
+		if (GetAsyncKeyState('A') & 0x8000)
+			p_trf->position.x -= 0.04f;
 
-		if (GetAsyncKeyState(VK_UP) & 0x8000)
-			p_trf->position.z += 0.001f;
-		if (GetAsyncKeyState(VK_DOWN) & 0x8000)
-			p_trf->position.z -= 0.001f;
+		if (GetAsyncKeyState('W') & 0x8000)
+			p_trf->position.z += 0.04f;
+		if (GetAsyncKeyState('S') & 0x8000)
+			p_trf->position.z -= 0.04f;
+
+		if (GetAsyncKeyState(VK_SPACE) & 0x8000)
+			p_trf->position.y += 0.04f;
+		if (GetAsyncKeyState(VK_SHIFT) & 0x8000)
+			p_trf->position.y -= 0.04f;
+
+		if (GetAsyncKeyState('Q') & 0x8000)
+			p_trf->rotation.y -= 0.03f;
+		if (GetAsyncKeyState('E') & 0x8000)
+			p_trf->rotation.y += 0.03f;
 
 		std::cout << "Camera3DPos : " << p_trf->position.x << " , " << p_trf->position.y << " , " << p_trf->position.z << std::endl;
 	}
 	{
 		Transform* p_trf = gameObject3D.GetTransformPtr();
 		p_trf->rotation.y += 0.000;
-		std::cout << "2DObjectPos : " << p_trf->position.x << " , " << p_trf->position.y << " , " << p_trf->position.z << std::endl;
+		std::cout << "3DObjectPos : " << p_trf->position.x << " , " << p_trf->position.y << " , " << p_trf->position.z << std::endl;
 	}
 
 
