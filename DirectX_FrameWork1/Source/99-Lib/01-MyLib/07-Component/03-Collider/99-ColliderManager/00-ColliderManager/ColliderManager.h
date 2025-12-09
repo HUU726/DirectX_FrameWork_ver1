20ffb714ceb57bac2aedc545b03167245bf6af21
huuki <huuki0726@icloud.com>
2025-12-09 22:13:26 +0900

    ColliderManagerのCollider同士のペア作成を効率化している最中・帰宅次第作業

--- DirectX_FrameWork1/Source/99-Lib/01-MyLib/07-Component/03-Collider/99-ColliderManager/00-ColliderManager/ColliderManager.h
+++ DirectX_FrameWork1/Source/99-Lib/01-MyLib/07-Component/03-Collider/99-ColliderManager/00-ColliderManager/ColliderManager.h
@@ -3,6 +3,28 @@
 #include "../../../99-CompMng/IF_ComponentManager.h"
 #include "../../00-Collider/Collider.h"
 #include <vector>
+#include <unordered_set>
+
+using ColliderPair = std::pair<Collider*, Collider*>;
+
+struct PairHash
+{
+	size_t operator()(const ColliderPair& _pair) const noexcept
+	{
+		return std::hash<Collider*>()(_pair.first) ^ (std::hash<Collider*>()(_pair.second) << 1);
+	}
+};
+
+struct PairEq
+{
+	bool operator()(const ColliderPair& _pairA, const ColliderPair& _pairB) const noexcept
+	{
+		return (_pairA.first == _pairB.first && _pairA.second == _pairB.second);
+	}
+};
+
+using PairSet = std::unordered_set<ColliderPair, PairHash, PairEq>;
+
 
 template<class T>
 class ColliderManager : public IF_ComponentManager
@@ -13,14 +35,14 @@ protected:
 	std::vector<T*> li_collider;
 	std::vector<T*> li_enableCol;
 
-	std::vector<std::pair<T*,T*>> li_curCollisionPair;
-	std::vector<std::pair<T*,T*>> li_preCollisionPair;
-
-
+	std::vector<ColliderPair> li_curCollisionPair;
+	std::vector<ColliderPair> li_preCollisionPair;
 
 	void OnCollisionEnter(Collider* _p_colliderA, Collider* _p_colliderB);
 	void OnCollisionStay(Collider* _p_colliderA, Collider* _p_colliderB);
 	void OnCollisionExit(Collider* _p_colliderA, Collider* _p_colliderB);
+
+	void CheckPair();
 
 	ColliderManager();
 
@@ -57,6 +79,13 @@ inline void ColliderManager<T>::OnCollisionExit(Collider* _p_colliderA, Collider
 {
 	_p_colliderA->OnCollisionExit(_p_colliderB);
 	_p_colliderB->OnCollisionExit(_p_colliderA);
+}
+
+template<class T>
+inline void ColliderManager<T>::CheckPair()
+{
+
+
 }
 
 template<class T>
