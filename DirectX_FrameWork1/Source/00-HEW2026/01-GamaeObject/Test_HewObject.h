a7f32b240e87ebb23b581f222acbedf8ab095054
huuki <huuki0726@icloud.com>
2025-12-10 12:29:34 +0900

    Hewでの作成オブジェクト追加フォルダを追加

---
+++ DirectX_FrameWork1/Source/00-HEW2026/01-GamaeObject/Test_HewObject.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../../99-Lib/01-MyLib/06-GameObject/01-2DGameObject/GameObject2D.h"
+
+
+class Test_HewObject : public GameObject2D
+{
+
+};
---
+++ DirectX_FrameWork1/Source/00-HEW2026/02-Scene/Test_HewScene.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "../../99-Lib/01-MyLib/08-Scene/01-Scenes/00-BaseScene/BaseScene.h"
+
+class Test_HewScene : public BaseScene
+{
+
+};
