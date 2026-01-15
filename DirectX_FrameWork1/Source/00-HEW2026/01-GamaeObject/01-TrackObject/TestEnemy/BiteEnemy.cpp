aeec8750edb4ee1961c09083cd7d98aa36236237
Rerua7 <ohs40772@ohs.hal.ac.jp>
2026-01-15 13:54:06 +0900

    ヘッダー追記

---
+++ DirectX_FrameWork1/Source/00-HEW2026/01-GamaeObject/01-TrackObject/TestEnemy/BiteEnemy.cpp
@@ -0,0 +1,4 @@
+#include "DirectX_FrameWork1/Source/99-Lib/01-MyLib/07-Component/03-Collider/01-Collider2D/BoxCollider2D.h"
+
+#include "../../../../99-Lib/01-MyLib/07-Component/06-Animator/01-SpriteAnimator/SpriteAnimator.h"
+#include "../../../../99-Lib/01-MyLib/07-Component/02-Renderer/01-SpriteRenderer/SpriteRenderer.h"
--- DirectX_FrameWork1/Source/00-HEW2026/01-GamaeObject/01-TrackObject/TestEnemy/BiteEnemy.h
+++ DirectX_FrameWork1/Source/00-HEW2026/01-GamaeObject/01-TrackObject/TestEnemy/BiteEnemy.h
@@ -1,1 +1,51 @@
 #pragma once
+#include"Enemy.h"
+
+class BiteEnemy : public CEnemy
+{
+private:
+	// 敵の情報
+	float hitstopspeed;	// HPが0になった際に止まる時間のスピード
+	float hitstoptime;	// ヒットストップがかかるフレーム数
+	hft::HFFLOAT3 Scale;	// 噛みつく敵の画像のサイズ
+
+	GameObject2D object2D;	// 本体のアニメーション
+
+	// 経過時間 
+	int timer;
+
+	// 状態別のフレーム数
+	int Idle_flame;
+	int Attack_flame;
+	int Spin_flame;
+	int Dead_flame;
+
+
+	// 向きに関する関数
+	void SetAngle(const hft::HFFLOAT2& NewAngle) { return; }
+	hft::HFFLOAT2 GetAngle() { return angle; }
+public:
+	BiteEnemy(const hft::HFFLOAT2&);	// 指定された座標にオブジェクトを作成,方向の初期化
+	~BiteEnemy();				// 解放処理
+
+	void Init() override;		// コンポーネント取得
+	void Update() override;		// 更新処理
+
+	// コライダーの状態を変更
+	void SetColliderActive(bool state);
+
+	// 状態毎の処理をする関数
+	void Normal_Move();	// 通常関数
+	void Attack_Move();	// 攻撃関数
+	void Spin_Move();	// 回転関数
+	void Dead_Move();	// 死亡関数
+
+	// ヒットストップ
+	void SetDelay();
+
+	// アニメーション
+	void Normal_Animation();
+	void Attack_Animation();
+	void Spin_Animation();
+	void Dead_Animation();
+};
