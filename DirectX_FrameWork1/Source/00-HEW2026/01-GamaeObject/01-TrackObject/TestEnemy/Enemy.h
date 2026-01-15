b26de539ec700ebfa1c8d34dd915a2c4225fb724
Rerua7 <ohs40772@ohs.hal.ac.jp>
2026-01-15 13:25:23 +0900

    Enemyクラス追加

--- DirectX_FrameWork1/Source/00-HEW2026/01-GamaeObject/01-TrackObject/TestEnemy/Enemy.h
+++ DirectX_FrameWork1/Source/00-HEW2026/01-GamaeObject/01-TrackObject/TestEnemy/Enemy.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include "../00-TrackObject/TrackObject.h"
+#include "../../../../01-MyLib/07-Component/06-Animator/01-SpriteAnimator/SpriteAnimator.h"
+
+
+class CEnemy : public TrackObject
+{
+private:
+	float e_flame = 0;	// フレーム数を格納する変数
+	int e_state = 0;	// 状態を表す変数
+	static int* cnt;		// エネミーの総数
+protected:
+	void SetState(const int& NewState) { e_state = NewState; }
+	int GetState() { return e_state; }
+	void SetFlame(const float& NewFlame){ e_flame = NewFlame; }
+	float GetFlame() { return  e_flame; }
+	void AddEnemyCount() { cnt++;}
+	void DeleteEnemyCount() { cnt--;}
+	int GetEnemyCount() { return *cnt; }
+};
