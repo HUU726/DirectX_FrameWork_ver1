18e023b9920963dd8e7c5b10c626e6d47a53c94a
AKASUNI <EikeyAK39@gmail.com>
2026-01-15 11:17:06 +0900

    マウスの侵入状態と押下状態検知

--- DirectX_FrameWork1/Source/00-HEW2026/01-GamaeObject/02-UI/UI.cpp
+++ DirectX_FrameWork1/Source/00-HEW2026/01-GamaeObject/02-UI/UI.cpp
@@ -3,33 +3,176 @@
 #include "../../../99-Lib/01-MyLib/07-Component/02-Renderer/01-SpriteRenderer/SpriteRenderer.h"
 #include "../../../99-Lib/01-MyLib/02-Renderer/99-ShapeTable/01-ShapeTable2D/ShapeTable2D.h"
 
+#include"../../../04-Input/Input.h"
+
 //シェーダーをUI用に変更
 UI::UI()
 {
+	////画像セット
+	//auto renderer = GetComponent<SpriteRenderer>();
+	//renderer->LoadTexture("Assets/01-Texture/99-Test/Tile.jpg");
+
+	//auto animator = AddComponent<SpriteAnimator>(hft::HFFLOAT2(0.999f, 0.999f));
+	////animator->Init();
+	//auto div = animator->GetDivision();
+	//SpriteAnimation anim(div, hft::HFFLOAT2(0, 0), 1);
+	//anim.SetType(SPRITE_ANIM_TYPE::LOOP);
+	//animator->AddAnimation(anim);
+	//
+	//animator->Play(0);
+
+
+	//シェーダーセット
+	auto& shader = GetComponent<SpriteRenderer>()->GetPolygonRef().material.shader;
+	auto VS_UI2D = hft::VertexShaderTable::GetInstance().CreateShader("Source/99-Lib/01-MyLib/999-Shader/01-2D/99-UI/VS_UI2D.hlsl");
+
+	shader.SetVertexShader(VS_UI2D);
+
+	////サイズセット
+	//auto tfm = GetComponent<Transform>();
+
+	//tfm->scale = hft::HFFLOAT2{ 300.f,150.f };
+	//tfm->position = hft::HFFLOAT3{0.f,0.f, -10.f};
+
+}
+
+void UI::Init(const hft::HFFLOAT3 pos, const hft::HFFLOAT2 scale, const char* texName, Type_UI type)
+{
+	//GameObject2D側が将来共通初期化処理になった時用
+	GameObject2D::Init();
+
 	//画像セット
 	auto renderer = GetComponent<SpriteRenderer>();
-	renderer->LoadTexture("Assets/01-Texture/99-Test/Tile.jpg");
+	renderer->LoadTexture(texName);
 
+	//uv座標を調整
 	auto animator = AddComponent<SpriteAnimator>(hft::HFFLOAT2(0.999f, 0.999f));
 	//animator->Init();
 	auto div = animator->GetDivision();
 	SpriteAnimation anim(div, hft::HFFLOAT2(0, 0), 1);
 	anim.SetType(SPRITE_ANIM_TYPE::LOOP);
 	animator->AddAnimation(anim);
-	
 	animator->Play(0);
 
-
-	//シェーダーセット
+	//UI用のシェーダーセット
 	auto& shader = renderer->GetPolygonRef().material.shader;
 	auto VS_UI2D = hft::VertexShaderTable::GetInstance().CreateShader("Source/99-Lib/01-MyLib/999-Shader/01-2D/99-UI/VS_UI2D.hlsl");
-
 	shader.SetVertexShader(VS_UI2D);
 
+
 	//サイズセット
+	initialScale = scale;
+
 	auto tfm = GetComponent<Transform>();
+	tfm->scale = scale;
+	tfm->position = pos;
 
-	tfm->scale = hft::HFFLOAT2{ 300.f,150.f };
-	tfm->position = hft::HFFLOAT3{0.f,0.f, -10.f};
+	this->type = type;
 
+
+}
+
+void UI::Update()
+{
+	//更新処理
+	switch (type)
+	{
+	case NormalType:
+
+		break;
+	case ButtonType:
+		UpdateIsMouseInside();
+		UpdateIsPressed();
+		break;
+	case DynamicType:
+
+		break;
+
+	default:
+		break;
+	}
+
+	//アニメーション系の処理
+	
+	//何もない場合は色とサイズを初期値に戻す
+	GetTransformPtr()->scale = initialScale;
+
+	if (isPressed)
+	{
+		AnimationPressed();
+	}
+	else if (isMouseInside)
+	{
+		AnimationMouseInside();
+	}
+
+}
+
+void UI::UpdateIsPressed()
+{
+	Input& input = Input::GetInstance();
+	bool isMousePressed = input.GetMousePress(Button::Mouse::Left) ||
+						  input.GetMousePress(Button::Mouse::Right) ||
+						  input.GetMousePress(Button::Mouse::Middle);
+
+	if (isMouseInside && isMousePressed)
+	{
+		isPressed = true;
+		std::cout << "UIがmouseに押されている" << std::endl;
+	}
+	else
+	{
+		isPressed = false;
+		std::cout << "UIがmouseに押されていない" << std::endl;
+	}
+}
+
+void UI::UpdateIsMouseInside()
+{
+	Input& input = Input::GetInstance();
+	float posX = input.GetMouseCenterX();
+	float posY = input.GetMouseCenterY();
+
+	hft::HFFLOAT2 mousePos = { posX, posY };
+
+	hft::HFFLOAT3 myPos = GetTransform().position;
+	hft::HFFLOAT2 mySize = GetTransform().scale;
+
+	float halfW = mySize.x * 0.5f;
+	float halfH = mySize.y * 0.5f;
+
+	bool isInside = mousePos.x >= myPos.x - halfW &&
+					mousePos.x < myPos.x + halfW &&
+					mousePos.y >= myPos.y - halfH &&
+					mousePos.y < myPos.y + halfH;
+
+	if (isInside)
+	{
+		isMouseInside = true;
+	}
+	else
+	{
+		isMouseInside = false;
+	}
+
+}
+
+bool UI::GetIsPressed()
+{
+	return isPressed;
+}
+
+bool UI::GetIsMouseInside()
+{
+	return isMouseInside;
+}
+
+void UI::AnimationPressed()
+{
+	GetTransformPtr()->scale = initialScale * 0.9f;
+}
+
+void UI::AnimationMouseInside()
+{
+	GetTransformPtr()->scale = initialScale * 1.1f;
 }
--- DirectX_FrameWork1/Source/00-HEW2026/01-GamaeObject/02-UI/UI.h
+++ DirectX_FrameWork1/Source/00-HEW2026/01-GamaeObject/02-UI/UI.h
@@ -1,12 +1,39 @@
 #pragma once
 #include "../../../99-Lib/01-MyLib/06-GameObject/01-2DGameObject/GameObject2D.h"
 
+
+//UIの種類
+enum Type_UI
+{
+	NormalType,	 //初期値から何も作用しない。背景など
+	ButtonType,  //押下を検知等
+	DynamicType, //アニメーションをするUI
+};
+
 class UI : public GameObject2D
 {
 private:
-
+	hft::HFFLOAT2 initialScale;		//初期の拡大率(アニメーション後の引き戻しで使う)
+	Type_UI type;					//UIの種類
+	bool isPressed = false;			//押されているかどうか
+	bool isMouseInside = false;		//マウスが自身の範囲内に存在するか
 public:
 	UI();
+	
+	//初期位置、サイズ、テクスチャ名、UIの種類を設定
+	void Init(const hft::HFFLOAT3 pos, const hft::HFFLOAT2 scale, const char* tex, Type_UI type);
+	
+	void Update() override;
 
+	//ボタンの押下状態を更新
+	void UpdateIsPressed();
+
+	//mouseが自身の範囲内にあるかの状態を更新
+	void UpdateIsMouseInside();
+
+	bool GetIsPressed();
+	bool GetIsMouseInside();
+
+	void AnimationPressed();
+	void AnimationMouseInside();
 };
-
