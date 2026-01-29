3fa8b02ce1e255ed2554f2da4e91e2f888145a0b
AKASUNI <EikeyAK39@gmail.com>
2026-01-29 16:35:53 +0900

    絶対パス修正

--- DirectX_FrameWork1/Source/00-HEW2026/01-GamaeObject/01-TrackObject/03_ConnectObject/ConnectObject.cpp
+++ DirectX_FrameWork1/Source/00-HEW2026/01-GamaeObject/01-TrackObject/03_ConnectObject/ConnectObject.cpp
@@ -1,7 +1,7 @@
 #include "ConnectObject.h"
 #include "ConnectObjectParam.h"
 
-#include "DirectX_FrameWork1/Source/99-Lib/01-MyLib/07-Component/03-Collider/01-Collider2D/BoxCollider2D.h"
+#include "../../../../99-Lib/01-MyLib/07-Component/03-Collider/01-Collider2D/BoxCollider2D.h"
 
 #include "../../../../99-Lib/01-MyLib/07-Component/06-Animator/01-SpriteAnimator/SpriteAnimator.h"
 #include "../../../../99-Lib/01-MyLib/07-Component/02-Renderer/01-SpriteRenderer/SpriteRenderer.h"
